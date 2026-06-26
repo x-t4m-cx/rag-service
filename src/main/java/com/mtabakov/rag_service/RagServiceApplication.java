1d477877fd3dbd863780731062b2207dc31bde5a
x-t4m-cx <t4bakovm@yandex.ru>
2026-06-26 15:17:00 +0400

    init commit

---
+++ src/main/java/com/mtabakov/rag_service/RagServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mtabakov.rag_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtabakov/rag_service/RagServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtabakov.rag_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
